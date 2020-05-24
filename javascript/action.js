0150ca2a2995ee1e62921687f56a7d99f136da47
dogukanarslan <dogukan2arslan@hotmail.com>
2020-05-24 16:23:17 +0300

    Increase readibility

--- javascript/action.js
+++ javascript/action.js
@@ -4,12 +4,21 @@ const film_name_input = document.querySelector('#film_name');
 const film_director_input = document.querySelector('#film_director');
 const delete_all_button = document.querySelector('#delete_all_button');
 
-form.addEventListener('submit', (event) => {
+const eventListeners = () => {
+    form.addEventListener('submit', addFilm);
+    delete_all_button.addEventListener('click', UI.deleteAllFilms);
+}
+
+const addFilm = (event) => {
     event.preventDefault();
     const film = new Film(film_name_input.value, film_director_input.value);
     UI.addFilm(event, film);
-    film_name_input.value = '';
-    film_director_input.value = '';
-});
 
-delete_all_button.addEventListener('click', UI.deleteAllFilms);
+    clearInputs(film_name_input, film_director_input);
+}
+
+const clearInputs = (...element) => {
+    element.forEach(element => element.value = '');
+}
+
+eventListeners();
