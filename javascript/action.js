121ae625b3d4803691d842972e50e1467e658817
dogukanarslan <dogukan2arslan@hotmail.com>
2020-05-24 13:59:51 +0300

    Enable delete option

--- javascript/action.js
+++ javascript/action.js
@@ -1,12 +1,14 @@
 const form = document.querySelector('#form');
 const films = document.querySelector('#films')
 const film_name_input = document.querySelector('#film_name');
-const film_director_input = document.querySelector('#film_director')
-
+const film_director_input = document.querySelector('#film_director');
+const delete_all_button = document.querySelector('#delete_all_button');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const film = new Film(film_name_input.value, film_director_input.value);
-    UI.addFilm(film);
+    UI.addFilm(event, film);
     film_name_input.value = '';
     film_director_input.value = '';
-});  
+});
+
+delete_all_button.addEventListener('click', UI.deleteAllFilms);
--- javascript/ui.js
+++ javascript/ui.js
@@ -1,16 +1,23 @@
 class UI {
-    static addFilm(film) {
+    static addFilm(event, film) {
         const film_element = document.createElement('li');
         const film_name_element = document.createElement('h4');
         const film_director_element = document.createElement('p');
+        const delete_film_element = document.createElement('button');
         film_name_element.innerHTML = film.name;
         film_director_element.innerHTML = film.director;
+        delete_film_element.type = 'button';
+        delete_film_element.innerHTML = 'Delete';
+        delete_film_element.addEventListener('click', (event) => event.target.parentElement.remove());
         film_element.appendChild(film_name_element);
         film_element.appendChild(film_director_element);
+        film_element.appendChild(delete_film_element);
         films.appendChild(film_element);
     }
 
-    static deleteFilm() {
-
+    static deleteAllFilms() {
+        while(films.firstElementChild) {
+            films.firstElementChild.remove();
+        }
     }
 }
