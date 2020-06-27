958a2fea843369f0fb0a1b5ded238dc7531d89d0
dogukanarslan <dogukan2arslan@hotmail.com>
2020-06-27 12:58:52 +0300

    Add necessary definitions

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
     mode: 'development',
     module: {
         rules: [
